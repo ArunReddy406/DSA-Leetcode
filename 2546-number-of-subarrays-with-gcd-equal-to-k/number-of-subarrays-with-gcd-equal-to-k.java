cb47e483717d196ca0e654af0630610de4f10eaa
Arun Reddy <arunreddykukudala07@gmail.com>
2025-08-17 23:47:48 +0530

    Time: 19 ms (15.82%) | Memory: 42.5 MB (15.31%) - LeetSync

---
+++ 2546-number-of-subarrays-with-gcd-equal-to-k/number-of-subarrays-with-gcd-equal-to-k.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int subarrayGCD(int[] nums, int k) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            int g=0;
+            for(int j=i;j<nums.length;j++){
+                g=hcf(g,nums[j]);
+                if(g==k){
+                    count++;
+                }
+                // else if(g<k){
+                //     break;
+                // }
+            }
+        }
+        return count;
+    }
+    public static int hcf(int a,int b){
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
+}
