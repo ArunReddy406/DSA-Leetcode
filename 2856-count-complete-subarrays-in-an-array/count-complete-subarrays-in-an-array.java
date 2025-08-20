6a0cd604c8c7269ff2551d4abc970569c7a74538
Arun Reddy <arunreddykukudala07@gmail.com>
2025-08-20 15:00:18 +0530

    Time: 360 ms (5.02%) | Memory: 45.1 MB (66.16%) - LeetSync

---
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countCompleteSubarrays(int[] nums) {
+        Set<Integer>set=new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+         int count=0;
+        for(int i=0;i<nums.length;i++){
+            HashSet<Integer>dup=new HashSet<>();
+            for(int j=i;j<nums.length;j++){
+                if(set.contains(nums[j])){
+                    dup.add(nums[j]);
+                    if(set.size()==dup.size()){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
