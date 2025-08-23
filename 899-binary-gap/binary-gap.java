dabdf2f86e3270429da714d3c6fe91ba6de4e7f3
Arun Reddy <arunreddykukudala07@gmail.com>
2025-08-23 11:04:49 +0530

    Time: 9 ms (2.51%) | Memory: 41.3 MB (6.52%) - LeetSync

---
+++ 899-binary-gap/binary-gap.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int binaryGap(int n) {
+        String s="";
+        while(n!=0){
+            int rem=n%2;
+            s=rem+s;
+            n/=2;
+        }
+        int count=0;
+        int maxi=0;
+        ArrayList<Integer>list=new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='1'){
+                list.add(i);
+            }
+        }
+        if(list.size()<=1){
+            return 0;
+        }
+        for(int i=0;i<list.size()-1;i++){
+            maxi=Math.max(maxi,list.get(i+1)-list.get(i));
+        }
+        // maxi=Math.max(maxi,count);
+        return maxi;
+    }
+}
