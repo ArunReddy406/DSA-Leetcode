2c8be026dc3dbc6185c0e190163b25ebe17829c6
Arun Reddy <arunreddykukudala07@gmail.com>
2025-09-02 00:01:34 +0530

    Time: 1 ms (59.43%) | Memory: 44.9 MB (62.00%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,22 +3,22 @@ class Solution {
         if(nums==null||nums.length==0){
                     return 0;
         }
-        // int k=1;
-        // for(int i=1;i<nums.length;i++){
-        //     if(nums[i]!=nums[i-1]){
-        //         nums[k]=nums[i];
-        //         k++;
-        //     }
+        int k=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[i-1]){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+        // Set<Integer>set=new TreeSet<>();
+        // for(int num:nums){
+        //     set.add(num);
         // }
-        // return k;
-        Set<Integer>set=new TreeSet<>();
-        for(int num:nums){
-            set.add(num);
-        }
-        int i=0;
-        for(int num:set){
-            nums[i++]=num;
-        }
-        return set.size();
+        // int i=0;
+        // for(int num:set){
+        //     nums[i++]=num;
+        // }
+        // return set.size();
     }
 }
