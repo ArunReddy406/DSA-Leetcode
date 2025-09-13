7f4f66ea3a0335f04d2ed318026c3b9f12d35e54
Arun Reddy <arunreddykukudala07@gmail.com>
2025-09-13 11:23:15 +0530

    Time: 4 ms (39.72%) | Memory: 43 MB (24.31%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxFreqSum(String s) {
+        HashMap<Character,Integer>map1=new HashMap<>();
+        HashMap<Character,Integer>map2=new HashMap<>();
+        int max=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                map1.put(ch,map1.getOrDefault(ch,0)+1);
+            }
+            else{
+                map2.put(ch,map2.getOrDefault(ch,0)+1);
+            }
+        }
+        if(map1.size()!=0){
+            max=Collections.max(map1.values());
+        }
+        if(map2.size()!=0){
+            max+=Collections.max(map2.values());
+        }
+        return max;
+    }
+}
