4d06f1ce144420be38ab79ed534408ac7cb78abe
Arun Reddy <arunreddykukudala07@gmail.com>
2025-09-14 11:41:11 +0530

    Time: 7 ms (100.00%) | Memory: 45.8 MB (0.00%) - LeetSync

--- 3997-maximize-sum-of-at-most-k-distinct-elements/maximize-sum-of-at-most-k-distinct-elements.java
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/maximize-sum-of-at-most-k-distinct-elements.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] maxKDistinct(int[] nums, int k) {
         HashSet<Integer>set=new HashSet<>();
-        int j=0;
         for(int i=0;i<nums.length;i++){
             set.add(nums[i]);
         }
@@ -10,9 +9,10 @@ class Solution {
         for(int arr:set){
             num[idx++]=arr;
         }
-        Arrays.sort(num);
         int len=Math.min(k,num.length);
         int ans[]=new int[len];
+        Arrays.sort(num);
+        int j=0;
         for(int i=num.length-1;i>=num.length-len;i--){
             ans[j++]=num[i];
         }
