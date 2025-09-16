0194da3e8758de00bb2de622f1734446a1ea1d41
Arun Reddy <arunreddykukudala07@gmail.com>
2025-09-16 22:34:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (99.36%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,14 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        int sum1=nums[0];
-        int sum2=Math.max(nums[0],nums[1]); 
-        for(int i=2;i<nums.length;i++){
-            int x = Math.max(nums[i] + sum1, sum2);
-            sum1 = sum2;
-            sum2 = x;
+        if(nums.length < 2){
+            return nums[0];
         }
-        int max=Math.max(sum1,sum2);
-        return max;
+        int arr[]=new int[nums.length];
+        arr[0]=nums[0];
+        arr[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+           arr[i]=Math.max(arr[i-2]+nums[i],arr[i-1]);
+        }
+        return arr[nums.length-1];
     }
 }
