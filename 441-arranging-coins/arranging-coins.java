8c5d13336e4a069bceda367ecd724896aba38458
Arun Reddy <arunreddykukudala07@gmail.com>
2025-09-19 22:27:20 +0530

    Time: 9 ms (13.33%) | Memory: 40.8 MB (85.41%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,9 +1,6 @@
 class Solution {
     public int arrangeCoins(int n) {
         int count=0;
-        if(n==1){
-            return 1;
-        }
         for(int i=1;i<=n;i++){
             if(n-i>=0){
                 count++;
