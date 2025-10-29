c41ed57b2586d3b288a77f099aa60c66a8a9c9d5
Arun Reddy <arunreddykukudala07@gmail.com>
2025-10-29 12:56:15 +0530

    Time: 33 ms (86.03%) | Memory: 56.2 MB (92.17%) - LeetSync

---
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int num:arr){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int nums[]=new int[map.size()];
+        int j=0;
+        for(int key:map.keySet()){
+            nums[j++]=map.get(key);
+        }
+        Arrays.sort(nums);
+        int mid=0;
+        int n=arr.length;
+        if(n%2==0){
+            mid=n/2;
+        }
+        else{
+            mid=(n+1)/2;
+        }
+        int sum=0;
+        int count=0;
+        for(int i=nums.length-1;i>=0;i--){
+            sum+=nums[i];
+            count++;
+            if(sum>=mid){
+                break;
+            }
+        }
+        return count;
+    }
+}
