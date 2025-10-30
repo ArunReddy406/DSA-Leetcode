3a439675c8853046c671ce96e97828d315f84ed2
Arun Reddy <arunreddykukudala07@gmail.com>
2025-10-30 13:37:56 +0530

    Time: 28 ms (6.53%) | Memory: 45.3 MB (5.52%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<k;i++){
             Arrays.sort(gifts);
             int end=gifts.length-1;
-            gifts[end]=(int)(Math.sqrt(gifts[end]));
+            gifts[end]=(int)Math.floor(Math.sqrt(gifts[end]));
         }
         for(int i=0;i<gifts.length;i++){
             sum+=gifts[i];
