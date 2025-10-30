fe2648e460eeb15b067164a52acbaf933aab9886
Arun Reddy <arunreddykukudala07@gmail.com>
2025-10-30 13:37:36 +0530

    Time: 28 ms (6.53%) | Memory: 44.9 MB (5.52%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+        long sum=0;
+        for(int i=0;i<k;i++){
+            Arrays.sort(gifts);
+            int end=gifts.length-1;
+            gifts[end]=(int)(Math.sqrt(gifts[end]));
+        }
+        for(int i=0;i<gifts.length;i++){
+            sum+=gifts[i];
+        }
+        return sum;
+    }
+}
