c4fbe6690adb4719661c76999a9561cb64b306ea
Arun Reddy <arunreddykukudala07@gmail.com>
2025-11-03 12:37:09 +0530

    Time: 13 ms (9.02%) | Memory: 91.3 MB (5.68%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -3,26 +3,26 @@ class Solution {
         int sum=0;
         StringBuilder sb=new StringBuilder(colors);
         ArrayList<Integer>list=new ArrayList<>();
-        for(int num:neededTime){
-            list.add(num);
-        }
+        // for(int num:neededTime){
+        //     list.add(num);
+        // }
         for(int i=0;i<sb.length()-1;i++){
             char ch=sb.charAt(i);
             char next=sb.charAt(i+1);
             if(ch==next){
-                // sum+=Math.min(neededTime[i],neededTime[i+1]);
-                // neededTime[i+1]=Math.max(neededTime[i],neededTime[i+1]);
-               if(list.get(i)<list.get(i+1)){
-                sum+=list.get(i);
-                list.remove(i);
-                sb.deleteCharAt(i);
-               }
-               else{
-                sum+=list.get(i+1);
-                list.remove(i+1);
-                sb.deleteCharAt(i+1);
-               }
-               i--;
+                sum+=Math.min(neededTime[i],neededTime[i+1]);
+                neededTime[i+1]=Math.max(neededTime[i],neededTime[i+1]);
+            //    if(list.get(i)<list.get(i+1)){
+            //     sum+=list.get(i);
+            //     list.remove(i);
+            //     sb.deleteCharAt(i);
+            //    }
+            //    else{
+            //     sum+=list.get(i+1);
+            //     list.remove(i+1);
+            //     sb.deleteCharAt(i+1);
+            //    }
+            //    i--;
             }
         }
         return sum;
