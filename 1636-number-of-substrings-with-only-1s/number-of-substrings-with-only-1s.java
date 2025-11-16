7aa6461ac25a516717dc38ae45ab84a63ac2139f
Arun Reddy <arunreddykukudala07@gmail.com>
2025-11-16 15:22:18 +0530

    Time: 7 ms (11.52%) | Memory: 46.2 MB (14.66%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -2,12 +2,12 @@ class Solution {
     public int numSub(String s) {
         int ans=0;
         int count=0;
-        int mod=10000_00007;
+        int mod=1000000007;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch=='1'){
                 count++;
-                ans=(ans%mod+count%mod)%mod;
+                ans=(ans+count)%mod;
             }
             else{
                 count=0;
