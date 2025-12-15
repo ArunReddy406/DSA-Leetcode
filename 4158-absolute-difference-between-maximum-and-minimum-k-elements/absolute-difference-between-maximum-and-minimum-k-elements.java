8bc7f288c4dda854c9cf8c5e366f27b4270c7724
Arun Reddy <arunreddykukudala07@gmail.com>
2025-12-15 22:15:55 +0530

    Time: 7 ms (0.00%) | Memory: 46.5 MB (100.00%) - LeetSync

--- 4158-absolute-difference-between-maximum-and-minimum-k-elements/absolute-difference-between-maximum-and-minimum-k-elements.java
+++ 4158-absolute-difference-between-maximum-and-minimum-k-elements/absolute-difference-between-maximum-and-minimum-k-elements.java
@@ -9,8 +9,6 @@ class Solution {
         for(int i=nums.length-1;i>=nums.length-k;i--){
             max+=nums[i];
         }
-        System.out.println(min);
-        System.out.println(max);
         int ans=Math.abs(max-min);
         return ans;
     }
