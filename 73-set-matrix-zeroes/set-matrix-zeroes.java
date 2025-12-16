6bfd99b5234b93aaa23dc6b66d28c5a62d1a61cd
Arun Reddy <arunreddykukudala07@gmail.com>
2025-12-16 23:54:32 +0530

    Time: 1 ms (62.01%) | Memory: 47.3 MB (31.13%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void setZeroes(int[][] matrix){
+    public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
         int row[]=new int[m];
@@ -7,9 +7,9 @@ class Solution {
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                row[i]=1;
-                col[j]=1;
-              }
+                    row[i]=1;
+                    col[j]=1;
+                }
             }
         }
         for(int i=0;i<m;i++){
