9b22f54eee80d535bc8b076758a26fc7f1ef42c0
Arun Reddy <arunreddykukudala07@gmail.com>
2025-12-25 21:57:13 +0530

    Time: 9 ms (12.48%) | Memory: 47 MB (71.67%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
@@ -4,23 +4,19 @@ class Solution {
         int row=strs.length;
         int col=strs[0].length();
         int f=0;
-        for(int i=0;i<col;i++){
-            for(int j=0;j<row-1;j++){
-                char ch=strs[j].charAt(i);
-                char ch1=strs[j+1].charAt(i);
+        for(int j=0;j<col;j++){
+            for(int i=0;i<row-1;i++){
+                char ch=strs[i].charAt(j);
+                char ch1=strs[i+1].charAt(j);
                 if(Character.compare(ch,ch1)>0){
                     f=1;
                     break;
                 }
-                // if(f==1){
-                //     count++;
-                //     f=0;
-                // }
             }
             if(f==1){
-                    count++;
-                    f=0;
-                }
+                count++;
+                f=0;
+            }
         }
         return count;
     }
