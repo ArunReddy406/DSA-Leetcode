dc245c010150c00586ba280d9b99f94ac1aaf643
Arun Reddy <arunreddykukudala07@gmail.com>
2026-02-06 11:52:19 +0530

    Time: 1 ms (98.74%) | Memory: 46.6 MB (71.07%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n=nums.length;
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                arr[i]=0;
+            }
+            else{
+                int idx=(i+nums[i])%n;
+                if(idx<0){
+                    idx+=n;
+                }
+                arr[i]=nums[idx];
+            }
+        }
+        return arr;
+    }
+}
